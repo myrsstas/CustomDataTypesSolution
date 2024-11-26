422cf146de0edda0937a2823afb891089b8bbfd5
Myrsini Stasinou <myrsinistas@gmail.com>
2024-11-26 18:27:39 +0200

    the description of the exercise was added and some corrections were made

--- CustomTypesSolution/Program.cs
+++ CustomTypesSolution/Program.cs
@@ -1,24 +1,44 @@
-﻿namespace CustomTypesSolution
+﻿
+/* Ασκηση 1.6: 
+Δημιουργήστε ένα console application το οποίο θα περιέχει ένα custom type με όνομα “CustomDataTypes”,
+το οποίο θα περιέχει properties με όλα τα primitive types. Όταν το app τρέξει, θα δείξει στην κονσόλα τις τιμές
+από όλα τα properties. Επίσης, θα περιλαμβάνει και μια μέθοδο η οποία θα προσθέτει 2 ακέραιους και θα επιστρέφει 
+το αποτέλεσμά της.
+ */
+
+namespace CustomTypesSolution
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            CustomDataTypes dataTypes = new CustomDataTypes();
-            dataTypes.IntegerNumber = 10;
-            dataTypes.LongNumber = 20L;
-            dataTypes.DecimalNumber = 20M;
-            dataTypes.FloatNumber = 15.0f;
-            dataTypes.DoubleNumber = 20d;
-            dataTypes.CharType = 'm';
-            dataTypes.BoolType = true;
-            dataTypes.additionOfIntegerAndDouble(dataTypes.IntegerNumber, dataTypes.DoubleNumber); 
+            CustomDataTypes dataTypes = new CustomDataTypes
+            {
+                IntegerNumber = 10,
+                LongNumber = 20L,
+                DecimalNumber = 20M,
+                FloatNumber = 15.0f,
+                DoubleNumber = 20d,
+                CharType = 'm',
+                BoolType = true,
+            };
+
+            Console.WriteLine($@"
+                       Integer : {dataTypes.IntegerNumber}
+                       Long : {dataTypes.LongNumber}
+                       Decimal : {dataTypes.DecimalNumber}
+                       Float : {dataTypes.FloatNumber}
+                       Double : {dataTypes.DoubleNumber}
+                       Char : {dataTypes.CharType}
+                       Bool : {dataTypes.BoolType}
+                       ");
+           
+            dataTypes.ΑdditionOfIntegerAndDouble(dataTypes.IntegerNumber, dataTypes.DoubleNumber); 
         }
     }
 
     class CustomDataTypes
     {
-        int integerNumber;
         long longNumber;
         decimal decimalNumber;
         float floatNumber;
@@ -32,17 +52,7 @@
             Console.WriteLine("Here starts the constructor...");
         }
 
-        public int IntegerNumber
-        {
-            get
-            {
-                return integerNumber;
-            }
-            set
-            {
-                integerNumber = value;
-            }
-        }
+        public int IntegerNumber { get; set; }
 
         public long LongNumber
         {
@@ -116,7 +126,7 @@
             }
         }
 
-        public void additionOfIntegerAndDouble(int integerNumber , double doubleNumber)
+        public void ΑdditionOfIntegerAndDouble(int integerNumber , double doubleNumber) //methods should be Pascal Casing
         {
             double answer = integerNumber + doubleNumber;
             Console.WriteLine($"if you add {integerNumber} and {doubleNumber} you get {answer}");
